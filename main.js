05d497fdfac1ae06a8ada1c84301b33cfccb7811
Krzysztof007 <kkos8@wp.pl>
2022-08-19 06:51:04 +0200

    another examples - arrow functions

--- main.js
+++ main.js
@@ -70,3 +70,94 @@ function Vessel(){
 
 const container = new Vessel();
 console.log(container.swim());
+
+//keyword this
+function drive(){
+  return `I am driving ${this.speed} m/h`;
+}
+
+const jack = {
+  speed: 68,
+  drive
+}
+
+console.log(jack.drive());
+
+//Adding an object to the function
+function giveColor(){
+  return `My favourite color is ${this.color}`;
+}
+
+const anna = {
+  color: 'grey'
+}
+
+const result = giveColor.bind(anna);
+console.log(result());
+console.log(giveColor.bind(anna)());
+console.log(giveColor.call(anna));
+
+//setTimeout with keyword this
+function Animal(){
+  setTimeout(function show(){
+    console.log(this);
+  }, 1000);
+}
+
+const zebra = new Animal();
+
+//arrow function returning keyword this
+const fish = {
+  weight: 98,
+  fishTalk: function(){
+    return this;
+  },
+  fishTalk1: () => {
+    return this;
+  }
+}
+
+console.log(fish.fishTalk1());
+console.log(fish.fishTalk());
+
+//arrow function, keyword this and callbacks
+
+const dog = {
+  race: 'bulldog',
+  run(){
+    setTimeout(() => {
+      console.log(this.race);
+    }, 1000);
+  }
+}
+
+dog.run();
+
+//arrow function does not execute its own execution context
+//do not use arrow function with constructor
+function Bike(){};
+Bike.prototype.drive = function(){
+  return this + ' hej1';
+}
+Bike.prototype.jump = () => {
+  return this + ' hej2';
+}
+
+const yellowBike = new Bike();
+console.log(yellowBike.drive());
+console.log(yellowBike.jump());
+
+//some callbacks funtions should be arrow function but some should not
+function inn(callback){
+  callback();
+}
+function outt(){
+  console.log(this + ' hej3');
+}
+inn(outt);
+
+
+//do not use arrow functions with event listeners
+//do not use arrow function for object methods
+//do not use arrow function for prototypes
+//do not use arrow function for constructors
